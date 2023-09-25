b2c9b5297c44c40b641a4e3ab7d4d885bc969fa2
YiFan <jiao1995cn@gmail.com>
2023-09-25 02:38:41 -0500

    Developer index welcome.

---
+++ AdvancedTopic_FinalProject/Controllers/DeveloperController.cs
@@ -0,0 +1,31 @@
+﻿using AdvancedTopicsAuthDemo.Areas.Identity.Data;
+using AdvancedTopicsAuthDemo.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AdvancedTopic_FinalProject.Controllers
+{
+    [Authorize(Roles = "Developer")]
+    public class DeveloperController : Controller
+    {
+        private readonly ILogger<DeveloperController> _logger;
+        private readonly ATAuthDemoContext _context;
+        private readonly UserManager<DemoUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly SignInManager<DemoUser> _signInManager;
+
+        public DeveloperController(ILogger<DeveloperController> logger, ATAuthDemoContext context, RoleManager<IdentityRole> roleManager, UserManager<DemoUser> userManager, SignInManager<DemoUser> signInManager)
+        {
+            _logger = logger;
+            _context = context;
+            _roleManager = roleManager;
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
